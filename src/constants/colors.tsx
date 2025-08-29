7b634366aa68bc12ced286864d1fc17700450b75
Diego <diegobc04@gmail.com>
2025-08-28 21:04:59 -0500

    adds design tokens

---
+++ src/constants/colors.tsx
@@ -0,0 +1,12 @@
+export const Colors = {
+  primary: "#166D6B",
+  lightGreen: "#A2D033",
+  offWhite: "#F9FBFB",
+  bgGreen: "#EDF4D8",
+  dark: "#4E4E4E",
+  surface: "#CCE6E3",
+  grey1: "#707070",
+  grey2: "#9DA6B2",
+  red: "#F00000",
+  pink: "#FFD0D0",
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#166D6B",
+        dark: "#4E4E4E",
+        surface: "#CCE6E3",
+        red: "#F00000",
+        pink: "#FFD0D0",
+        "light-green": "#A2D033",
+        "off-white": "#F9FBFB",
+        "bg-green": "#EDF4D8",
+        "grey-1": "#707070",
+        "grey-2": "#9DA6B2",
+      },
+    },
   },
   plugins: [],
 };
